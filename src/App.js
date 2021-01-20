1bc5eb422687c020d677fa4fae3238389d18bad8
Claire Stolp <clairemstolp@gmail.com>
2021-01-19 22:15:26 -0500

    remove provider from app.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import {Provider} from 'react-redux';
 function App() {
   return (
     <>
-      <Provider>
         <header>
           <Header />
         </header>
         <Container>
             <RenderRoutes routes={ROUTES} />
         </Container>
-      </Provider> 
     </>
   );
 }
